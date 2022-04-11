c64efbb61802ffddf38663e16cb5addd8f6eb21d
Ioanna <ioanna.kozeljp@gmail.com>
2022-04-11 17:42:02 +0200

    coverlet

--- TestFolder/TestClass.cs
+++ TestFolder/TestClass.cs
@@ -2,13 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace minesweep.TestFolder
+namespace minesweep
 {
+    [TestClass]
     public class TestClass
     {
+        [TestMethod]
          public void TestDisplayMethod () {
-             //Assert.AreEqual(str, ui.DisplayTitle(), "Display Title not shown"); 
+             Display display = new Display();
+              string intro = "▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n"+
+                             "██ ▄▀▄ ██▄██ ▄▄▀█ ▄▄██ ▄▄▄ █ ███ █ ▄▄█ ▄▄█▀▄▄▀█ ▄▄█ ▄▄▀██\n"+
+                             "██ █ █ ██ ▄█ ██ █ ▄▄██▄▄▄▀▀█▄▀ ▀▄█ ▄▄█ ▄▄█ ▀▀ █ ▄▄█ ▀▀▄██\n"+
+                             "██ ███ █▄▄▄█▄██▄█▄▄▄██ ▀▀▀ ██▄█▄██▄▄▄█▄▄▄█ ████▄▄▄█▄█▄▄██\n"+
+                             "▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀ ";  
+             Assert.AreEqual(intro, display.Intro(), "Display Title not shown"); 
 
          }
         
